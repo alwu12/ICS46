94678be3a51f8dce9ed1635009bd2858aceb527c
“Alvin” <alvinyw1@uci.edu>
2023-10-03 20:52:15 -0700

    finished verifySol.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,5 @@
-
+#include <iostream>
+using namespace std;
 // Note:  you are not required to implement a main() for this project.
 //      Feel free to create one; it will not be accounted for in grading.
 //      Be sure to use the unit testing process to test your code: that is
@@ -6,6 +7,7 @@
 
 int main()
 {
+    cout << "hello world" << endl;
     return 0;
 }
 
