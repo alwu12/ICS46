9bbbd8a473d86e73f588d308a719397a93b5f6de
“Alvin” <alvinyw1@uci.edu>
2023-10-06 15:28:19 -0700

    got rid of using namespace std

--- app/verifySol.cpp
+++ app/verifySol.cpp
@@ -1,14 +1,13 @@
 #include <unordered_map>
 #include <string>
 #include "verifySol.hpp"
-using namespace std;
 
-unsigned numberGenerator(string s, const std::unordered_map<char, unsigned> & map){
+unsigned numberGenerator(std::string s, const std::unordered_map<char, unsigned> & map){
     //translate a string to a number based on the given map
-    string num_holder;
-    string final;
+    std::string num_holder;
+    std::string final;
     for(auto &ch : s){ 
-        num_holder = to_string(map.find(ch)->second); //find the unsigned int in the map and change it to a string
+        num_holder = std::to_string(map.find(ch)->second); //find the unsigned int in the map and change it to a string
         final+=num_holder; //concatenate to the end of the final number
     }
     return stoul(final); //translate from string to int
